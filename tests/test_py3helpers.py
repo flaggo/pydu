cffe1d87d9a7c8f093a6551035d342d661e49850
Prodesire <Prodesire@yeah.net>
2017-10-01 22:37:44 +0800

    add testcase for py3helpers

---
+++ tests/test_py3helpers.py
@@ -0,0 +1,34 @@
+from pylib.py3helpers import (iterkeys, itervalues, iteritems, is_iter,
+                              text_type, string_types, numeric_types)
+
+
+def test_iter():
+    d = dict(a=1, b=2)
+    for key in iterkeys(d):
+        assert key in ('a', 'b')
+
+    for value in itervalues(d):
+        assert value in (1, 2)
+
+    for items in iteritems(d):
+        assert items in (('a', 1), ('b', 2))
+
+    assert is_iter(iter([]))
+
+
+def test_types():
+    assert isinstance(u'a', text_type)
+
+    assert isinstance(u'a', string_types)
+    assert isinstance('a', string_types)
+
+    assert isinstance(1, numeric_types)
+    assert isinstance(2**50, numeric_types)
+
+
+def test_urljoin():
+    from pylib.py3helpers import urljoin
+
+
+def test_imap():
+    from pylib.py3helpers import imap
