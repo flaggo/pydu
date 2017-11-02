76f94bb4feb99cf7b7cffd885563e200e626c168
Prodesire <Prodesire@yeah.net>
2017-11-02 22:53:29 +0800

    change " to """ in itercompat.is_iterable

--- pydu/itercompat.py
+++ pydu/itercompat.py
@@ -1,5 +1,5 @@
 def is_iterable(x):
-    "An implementation independent way of checking for iterables"
+    """An implementation independent way of checking for iterables."""
     try:
         iter(x)
     except TypeError:
