cdf79302e37142f618a3649f7833049ad20efbe5
Prodesire <prodesire@yeah.net>
2017-11-18 10:07:49 +0800

    rename "todo" to "to do" of conf annotation

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# If true, `todo` and `todoList` produce output, else they produce nothing.
+# If true, `to do` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
 
