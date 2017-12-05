a7e3e4a0b8090895bc3b886a0413a7b6faf95304
Prodesire <prodesire@yeah.net>
2017-12-05 22:50:24 +0800

    mv WINDOWS and POSIX from __init__ to platform

--- pydu/__init__.py
+++ pydu/__init__.py
@@ -8,7 +8,3 @@ __version__ = '0.0.2'
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
-
-
-WINDOWS = os.name == 'nt'
-POSIX = os.name == 'posix'
