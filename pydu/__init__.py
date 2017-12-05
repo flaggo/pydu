bb37d477276ab0b575b7d6c122381b5fd518709d
Prodesire <prodesire@yeah.net>
2017-12-05 21:36:43 +0800

    add logger to pydu

--- pydu/__init__.py
+++ pydu/__init__.py
@@ -1,7 +1,13 @@
 import os
+import logging
 
 
 __version__ = '0.0.2'
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 WINDOWS = os.name == 'nt'
