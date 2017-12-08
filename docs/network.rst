ed8486e2a163d57ca8bb931422b98a16e5aea827
Prodesire <prodesire@yeah.net>
2017-12-08 22:54:47 +0800

    add network.is_ipv6

--- docs/network.rst
+++ docs/network.rst
@@ -24,6 +24,17 @@ Network
     False
 
 
+.. py:function:: pydu.network.is_ipv6(ip)
+
+    Judge whether the given ``ip`` is IPV6 address.
+
+    >>> from pydu.network import is_ipv6
+    >>> is_ipv4('fe80::9e5b:b149:e187:1a18')
+    True
+    >>> is_ipv4('localhost.localdomain')
+    False
+
+
 .. py:function:: pydu.network.get_free_port()
 
     Get free port which could be bound.
--- tests/test_network.py
+++ tests/test_network.py
@@ -1,5 +1,5 @@
 import pytest
-from pydu.network import dotted_netmask, is_ipv4, get_free_port
+from pydu.network import dotted_netmask, is_ipv4, is_ipv6, get_free_port
 
 
 @pytest.mark.parametrize(
@@ -20,6 +20,16 @@ class TestIsIPv4Address:
     @pytest.mark.parametrize('value', ('8.8.8.8.8', 'localhost.localdomain'))
     def test_invalid(self, value):
         assert not is_ipv4(value)
+
+
+class TestIsIPv6Address:
+
+    def test_valid(self):
+        assert is_ipv6('fe80::9e5b:b149:e187:1a18')
+
+    @pytest.mark.parametrize('value', ('fe80::9e5b:b149:e187::', 'localhost.localdomain'))
+    def test_invalid(self, value):
+        assert not is_ipv6(value)
 
 
 def test_get_free_port():
