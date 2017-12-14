6565f5b553a12ea08a31def8bd7b8d88d839c4cd
Prodesire <prodesire@yeah.net>
2017-12-14 20:48:15 +0800

    improve information of setup

--- setup.py
+++ setup.py
@@ -5,9 +5,25 @@ from setuptools import setup, find_packages
 setup(
     name="pydu",
     version=__version__,
-    description="pydu",
+    description="Useful data structures, utils for Python.",
     author="Prodesire",
+    author_email='wangbinxin001@126.com',
+    license='MIT License',
     url="https://github.com/Prodesire/pydu",
     setup_requires=['pytest-runner'],
     packages=find_packages(),
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
 )
