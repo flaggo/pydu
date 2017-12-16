4354f43420cf429fe8cbe018c194d9eff3ecabd2
Prodesire <prodesire@yeah.net>
2017-12-16 22:55:54 +0800

    Set logging handler to avoid "No handler found" warnings.

--- pydu/__init__.py
+++ pydu/__init__.py
@@ -1,7 +1,14 @@
-import logging
-
+"""
+Useful data structures, utils for Python.
+"""
 
 __version__ = '0.1.0'
 
 
+# Set logging handler to avoid "No handler found" warnings.
+import logging
+from logging import NullHandler
+
+
 logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
