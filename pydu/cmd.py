0c02f3939140bb6efd287169c27d8dba8a3ab845
Prodesire <prodesire@yeah.net>
2017-12-23 11:37:21 +0800

    add env to cmd.run and cmd.run_with_en_env

--- pydu/cmd.py
+++ pydu/cmd.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import subprocess
@@ -38,7 +39,7 @@ else:
     TimeoutExpired = subprocess.TimeoutExpired
 
 
-def run(cmd, wait=True, shell=False, timeout=None, timeinterval=1):
+def run(cmd, wait=True, shell=False, env=None, timeout=None, timeinterval=1):
     """
     Run cmd based on `subprocess.Popen`.
     If `wait` is True, `run` will return the tuple of `(returncode, stdout)`.
@@ -48,7 +49,7 @@ def run(cmd, wait=True, shell=False, timeout=None, timeinterval=1):
     If the process does not terminate after `timeout` seconds, a `TimeoutExpired` exception will be raised.
     `timeinterval` is workable when timeout is given on Python 2. It means process status checking interval.
     """
-    p = Popen(cmd, shell=shell, stdout=PIPE, stderr=STDOUT)
+    p = Popen(cmd, shell=shell, stdout=PIPE, stderr=STDOUT, env=env)
     if not wait:
         return p
 
@@ -67,7 +68,7 @@ def run(cmd, wait=True, shell=False, timeout=None, timeinterval=1):
         return p.poll(), stdout
 
 
-def run_with_en_env(cmd, wait=True, shell=False, timeout=None, timeinterval=1):
+def run_with_en_env(cmd, wait=True, env=None, shell=False, timeout=None, timeinterval=1):
     """
     Run cmd with English character sets environment, so that the output will
     be in English.
@@ -78,12 +79,9 @@ def run_with_en_env(cmd, wait=True, shell=False, timeout=None, timeinterval=1):
             return run(cmd, wait=wait, shell=shell,
                        timeout=timeout, timeinterval=timeinterval)
     else:
-        export_lang = 'export LANG=en_US.UTF-8'
-        if isinstance(cmd, list):
-            pre_cmd = [export_lang, ';']
-        else:
-            pre_cmd = export_lang + '; '
-        return run(pre_cmd + cmd, wait=wait, shell=shell,
+        env = env if env else os.environ.copy()
+        env.update({'LANG': 'en_US.UTF-8'})
+        return run(cmd, wait=wait, shell=shell, env=env,
                    timeout=timeout, timeinterval=timeinterval)
 
 
