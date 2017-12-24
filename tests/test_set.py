1fff461708ea25e78a2a811ba396fa375c9b1cd6
Prodesire <prodesire@yeah.net>
2017-12-24 23:45:04 +0800

    improve test for set

--- tests/test_set.py
+++ tests/test_set.py
@@ -7,6 +7,12 @@ def test_ordered_set():
     assert 1 in ordered_set
     assert bool(ordered_set)
 
+    ordered_set.add(1)
+    assert 1 in ordered_set
+
+    ordered_set.remove(1)
+    assert 1 not in ordered_set
+
     for i in range(4):
         ordered_set.discard(i)
     assert not bool(ordered_set)
