891c5a3f6130e40f17ef4e1d3398d6eb2ecf192b
xuzg <xzg.nju@gmail.com>
2018-01-03 20:08:48 +0800

    Add test for random ip

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,5 +1,8 @@
 import pytest
-from pydu.network import dotted_netmask, is_ipv4, is_ipv6, get_free_port
+from collections import Iterator
+from pydu.network import (
+    dotted_netmask, is_ipv4,
+    is_ipv6, get_free_port,random_ip)
 
 
 @pytest.mark.parametrize(
@@ -36,3 +39,12 @@ def test_get_free_port():
     port = get_free_port()
     assert isinstance(port, int)
     assert 65536 > port > 0
+
+
+def test_random_ip():
+    ip_list = random_ip('10.1.100.2/27')
+    assert isinstance(ip_list, Iterator)
+    assert len(list(ip_list)) == 32
+    ip_list = random_ip('10.1.100.2/15')
+    assert isinstance(ip_list, Iterator)
+    assert len(list(ip_list)) == 131072
