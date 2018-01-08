0ada2fa96cd5f9858e49f23920fb4c9941968636
Prodesire <prodesire@yeah.net>
2018-01-08 21:11:25 +0800

    add boolean to string

--- tests/test_string.py
+++ tests/test_string.py
@@ -1,6 +1,7 @@
 # coding: utf-8
+import pytest
 from pydu.string import (safeencode, safeunicode, strips, lstrips, rstrips,
-                         common_prefix, common_suffix, sort)
+                         common_prefix, common_suffix, sort, boolean)
 
 
 def test_safeencode():
@@ -53,3 +54,26 @@ def test_common_suffix():
 def test_sort():
     assert sort('acb21') == '12abc'
     assert sort('abc21', reverse=True) == 'cba21'
+
+
+class TestBoolean:
+    def test_accepted_text(self):
+        for text in ('yes', 'y', 'on', 'true', 't', '1'):
+            assert boolean(text)
+            assert boolean(text.upper())
+
+        for text in ('no', 'n', 'off', 'false', 'f', '0'):
+            assert not boolean(text)
+            assert not boolean(text.upper())
+
+    @pytest.mark.parametrize('text', ('a', 'b'))
+    def test_unaccepted_text(self, text):
+        with pytest.raises(ValueError):
+            boolean(text)
+
+    def test_nonstring(self):
+        for obj in (10, [1], {1: 1}):
+            assert boolean(obj)
+
+        for obj in (0, [], {}):
+            assert not boolean(obj)
