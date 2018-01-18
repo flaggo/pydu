5d744f0873e5c699db7beb3ad66033c6f08f201f
Prodesire <prodesire@yeah.net>
2018-01-18 18:44:25 +0800

    add test for list.tolist

--- tests/test_list.py
+++ tests/test_list.py
@@ -1,5 +1,11 @@
-from pydu.list import uniq
+import pytest
+from pydu.list import uniq, tolist
 
 
 def test_uniq():
     assert uniq([1, 4, 0, 2, 0, 3]) == [1, 4, 0, 2, 3]
+
+
+@pytest.mark.parametrize('obj', ('foo', ['foo']))
+def test_tolist(obj):
+    assert tolist(obj) == ['foo']
