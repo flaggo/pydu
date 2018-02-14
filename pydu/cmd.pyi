e2624dfc203f146a39d45cb05fa68e3161857cd4
Prodesire <prodesire@yeah.net>
2018-02-14 20:51:45 +0800

    improve type hint for cmd

--- pydu/cmd.pyi
+++ pydu/cmd.pyi
@@ -1,12 +1,23 @@
 """Stubs for cmd"""
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, Union
 
 
 class TimeoutExpired(Exception):
 
-    def __init__(self, cmd: str, timeout: int, output: Optional[str]=..., stderr: Optional[str]=...) -> None: ...
+    def __init__(self, cmd: str,
+                 timeout: Union(int, float),
+                 output: Optional[str]=...,
+                 stderr: Optional[str]=...) -> None: ...
 
-def run(cmd: str, shell: bool=..., env: Optional[dict]=..., timeout: int=..., timeinterval: int=...) -> Tuple[int, str]: ...
-def run_with_en_env(cmd: str, shell: bool=..., env: Optional[dict]=..., timeout: int=..., timeinterval: int=...) -> Tuple[int, str]: ...
+def run(cmd: str,
+        shell: bool=...,
+        env: Optional[dict]=...,
+        timeout: Union(int, float)=...,
+        timeinterval: Union(int, float)=...) -> Tuple[int, str]: ...
+def run_with_en_env(cmd: str,
+                    shell: bool=...,
+                    env: Optional[dict]=...,
+                    timeout: Union(int, float)=...,
+                    timeinterval: Union(int, float)=...) -> Tuple[int, str]: ...
 def terminate(pid: int) -> None: ...
 def cmdline_argv() -> List: ...
