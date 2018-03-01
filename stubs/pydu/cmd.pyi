baad7f8d55f3057ccae689057e4a52aff8fadedf
Prodesire <prodesire@yeah.net>
2018-03-01 18:43:43 +0800

    fix Union syntax error

--- stubs/pydu/cmd.pyi
+++ stubs/pydu/cmd.pyi
@@ -5,19 +5,19 @@ from typing import Optional, Tuple, List, Union
 class TimeoutExpired(Exception):
 
     def __init__(self, cmd: str,
-                 timeout: Union(int, float),
+                 timeout: Union[int, float],
                  output: Optional[str]=...,
                  stderr: Optional[str]=...) -> None: ...
 
 def run(cmd: str,
         shell: bool=...,
         env: Optional[dict]=...,
-        timeout: Union(int, float)=...,
-        timeinterval: Union(int, float)=...) -> Tuple[int, str]: ...
+        timeout: Union[int, float]=...,
+        timeinterval: Union[int, float]=...) -> Tuple[int, str]: ...
 def run_with_en_env(cmd: str,
                     shell: bool=...,
                     env: Optional[dict]=...,
-                    timeout: Union(int, float)=...,
-                    timeinterval: Union(int, float)=...) -> Tuple[int, str]: ...
+                    timeout: Union[int, float]=...,
+                    timeinterval: Union[int, float]=...) -> Tuple[int, str]: ...
 def terminate(pid: int) -> None: ...
 def cmdline_argv() -> List: ...
