895dc8df6412faa714be3d0b7cfacf99ef9f4135
Prodesire <prodesire@yeah.net>
2018-04-25 19:36:55 +0800

    add exception.default_if_except A exception decorator which excepts given exceptions and return default value.

--- pydu/exception.py
+++ pydu/exception.py
@@ -1,4 +1,5 @@
 import contextlib
+from functools import wraps
 from .compat import PY2
 
 
@@ -11,3 +12,19 @@ if PY2:
             pass
 else:
     ignore = contextlib.suppress
+
+
+def default_if_except(exception_clses, default=None):
+    """
+    A exception decorator which excepts given exceptions and
+    return default value.
+    """
+    def _default_if_except(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except exception_clses:
+                return default
+        return wrapper
+    return _default_if_except
