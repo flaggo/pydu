272fc58b410e82fdc9a90b1e756eb6ae0f15e85c
Prodesire <prodesire@yeah.net>
2018-04-26 19:38:53 +0800

    add test for exception.default_if_except

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -1,7 +1,15 @@
-from pydu.exception import ignore
+from pydu.exception import ignore, default_if_except
 
 
 def test_ignore():
     with ignore(ValueError, AttributeError):
         int('abc')
         int.no_exists_func()
+
+
+def test_default_if_except():
+    @default_if_except(ValueError, default=0)
+    def foo():
+        return int('abc')
+
+    assert foo() == 0
