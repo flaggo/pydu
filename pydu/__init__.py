b2656e4019905d0c568309ff5b9f827bc806998e
Prodesire <prodesire@yeah.net>
2018-05-01 20:37:43 +0800

    start development for v0.7.0

--- pydu/__init__.py
+++ pydu/__init__.py
@@ -3,7 +3,7 @@ Useful data structures, utils for Python.
 """
 from __future__ import absolute_import
 
-__version__ = '0.6.2'
+__version__ = '0.7.0'
 
 
 # Set logging handler to avoid "No handler found" warnings.
