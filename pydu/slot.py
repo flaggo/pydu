59e205966ff1e8289331bb160b80624ae4754e46
Prodesire <prodesire@yeah.net>
2018-05-02 21:23:51 +0800

    add slot.SlotBase which is the base class for class using __slots__.

---
+++ pydu/slot.py
@@ -0,0 +1,19 @@
+from itertools import izip
+from .compat import iteritems
+
+
+class SlotBase(object):
+    """
+    Base class for class using __slots__.
+    If some args or kwargs are not given when initialize class,
+    the value of them will be set with ``None``.
+    """
+    def __init__(self, *args, **kwargs):
+        setted = set()
+        kwargs_ = dict(izip(self.__slots__, args))
+        kwargs_.update(kwargs)
+        for key, value in iteritems(kwargs_):
+            setattr(self, key, value)
+            setted.add(key)
+        for key in set(self.__slots__) - setted:
+            setattr(self, key, None)
